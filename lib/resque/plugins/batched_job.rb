afed900064feefa254c8c8abbee66c53d84ccc33
Misha Conway <mishaAconway@gmail.com>
2017-01-10 11:52:12 -0800

    updates

--- lib/resque/plugins/batched_job.rb
+++ lib/resque/plugins/batched_job.rb
@@ -1,21 +1,18 @@
 module Resque
-
   def assemble_batched_jobs id
     redis.set batch_in_progress_key(id), 1
     yield
     redis.del batch_in_progress_key(id)
   end
 
+  def batch_count_key(id)
+    "#{id}_count"
+  end
+
   def batch_in_progress_key(id)
     "#{id}_in_progress"
   end
 
-  # This is a small wrapper around Resque.enqueue.
-  # @param [Class] klass Job class.
-  # @param [Object, #to_s] bid Batch identifier.
-  def self.enqueue_batched_job(klass, bid, *args)
-    Resque.enqueue(klass, bid, *args)
-  end
 
   module Plugin
 
@@ -42,9 +39,7 @@ module Resque
       #
       # @param [Object, #to_s] id Batch identifier. Any Object that responds to #to_s
       def after_enqueue_batch(id, *args)
-        mutex(id) do |bid|
-          redis.rpush(bid, Resque.encode(:class => self.name, :args => args))
-        end
+        redis.incr Resque::batch_count_key(id)
       end
 
       # After the job is performed, remove it from the batched job list.  If the
@@ -53,8 +48,7 @@ module Resque
       #
       # @param id (see Resque::Plugins::BatchedJob#after_enqueue_batch)
       def after_perform_batch(id, *args)
-        if remove_batched_job(id, *args) == 0 && !batch_assembly_in_progress?(id)
-          
+        if batch_complete?(id) && !batch_assembly_in_progress?(id)
           after_batch_hooks = Resque::Plugin.after_batch_hooks(self)
           after_batch_hooks.each do |hook|
             send(hook, id, *args)
@@ -66,7 +60,7 @@ module Resque
       #
       # @param id (see Resque::Plugins::BatchedJob#after_enqueue_batch)
       def after_dequeue_batch(id, *args)
-        remove_batched_job(id, *args)
+        redis.decr Resque::batch_count_key(id)
       end
 
       # Checks the size of the batched job list and returns true if the list is
@@ -74,18 +68,7 @@ module Resque
       #
       # @param id (see Resque::Plugins::BatchedJob#batch)
       def batch_complete?(id)
-        mutex(id) do |bid|
-          redis.llen(bid) == 0
-        end
-      end
-
-      # Check to see if the Redis key exists.
-      #
-      # @param id (see Resque::Plugins::BatchedJob#batch)
-      def batch_exist?(id)
-        mutex(id) do |bid|
-          redis.exists(bid)
-        end
+        redis.get(Resque::batch_count_key(id)).to_i.zero?
       end
 
       def batch_assembly_in_progress?(id)
@@ -93,49 +76,6 @@ module Resque
       end
 
       # Remove a job from the batch list. (closes #6)
-      #
-      # @param id (see Resque::Plugins::BatchedJob#after_enqueue_batch)
-      def remove_batched_job(id, *args)
-        mutex(id) do |bid|
-          removed_count = redis.lrem(bid, 1, Resque.encode(:class => self.name, :args => args))
-
-          raise "Failed to remove batched job, id: #{id}, args: #{args.join(', ')}  remove count: #{removed_count}" if removed_count != 1
-
-          redis.llen(bid)
-        end
-      end
-
-      # Remove a job from the batch list and run after hooks if necessary.
-      #
-      # @param id (see Resque::Plugins::BatchedJob#remove_batched_job)
-      def remove_batched_job!(id, *args)
-        after_perform_batch(id, *args)
-      end
-
-      # Build a collection of Resque::Job objects that represent each job in a 
-      # batch of the same class.
-      #
-      # @param id (see Resque::Plugins::BatchedJob#remove_batched_job)
-      # @returns [Array] Collection of Resque::Job objects
-      def batched_jobs(id)
-        bid = batch(id)
-        regexp = /\A\{\"class\":\"#{self.name}\",\"args\":\[/
-        redis.lrange(bid, 0, redis.llen(bid)-1).grep(regexp).map do |string|
-          payload = Resque.decode(string)
-          payload['args'].unshift(id)
-          Resque::Job.new(@queue, payload)
-        end
-      end
-
-      # Collect and recreate all jobs for the given batch.
-      #
-      # @param id (see Resque::Plugins::BatchedJob#remove_batched_job)
-      # @returns [Integer] Number of jobs recreated.
-      def recreate_batched_jobs(id)
-        batched_jobs(id).each do |job|
-          job.recreate
-        end.size
-      end
 
       private
 
@@ -143,54 +83,7 @@ module Resque
         def redis
           Resque.respond_to?(:redis) ? Resque.redis : Resque.backend.store
         end
-
-        # Lock a batch key before executing Redis commands.  This will ensure
-        # no race conditions occur when modifying batch information.  Here is
-        # an example of how this works.  See http://redis.io/commands/setnx for
-        # more information. (fixes #4) (closes #5)
-        #
-        # * Job2 sends SETNX batch:123:lock in order to aquire a lock.
-        # * Job1 still has the key locked, so Job2 continues into the loop.
-        # * Job2 sends GET to aquire the lock timestamp.
-        # * If the timestamp does not exist (Job1 released the lock), Job2
-        #   attemps to start from the beginning again.
-        # * If the timestamp exists and has not expired, Job2 sleeps for a
-        #   moment and then retries from the start.
-        # * If the timestamp exists and has expired, Job2 sends GETSET to aquire
-        #   a lock.  This returns the previous value of the lock.
-        # * If the previous timestamp has not expired, another process was faster
-        #   and aquired the lock.  This means Job2 has to start from the beginnig.
-        # * If the previous timestamp is still expired the lock has been set and
-        #   processing can continue safely
-        #
-        # @param id (see Resque::Plugins::BatchedJob#batch)
-        # @yield [bid] Yields the current batch id.
-        # @yieldparam [String] The current batch id.
-        def mutex(id, &block)
-          is_expired = lambda do |locked_at|
-            locked_at.to_f < Time.now.to_f
-          end
-          bid   = batch(id)
-          _key_ = "#{bid}:lock"
-
-          until redis.setnx(_key_, Time.now.to_f + 0.5)
-            next unless timestamp = redis.get(_key_)
-
-            unless is_expired.call(timestamp)
-              sleep(0.1)
-              next
-            end
-
-            break unless timestamp = redis.getset(_key_, Time.now.to_f + 0.5)
-            break if is_expired.call(timestamp)
-          end
-          yield(bid)
-        ensure
-          redis.del(_key_)
-        end
-
     end
-
   end
 
 end
--- lib/resque/plugins/batched_job/version.rb
+++ lib/resque/plugins/batched_job/version.rb
@@ -1,7 +1,7 @@
 module Resque
   module Plugins
     module BatchedJob
-      VERSION = '1.9.4'
+      VERSION = '1.9.5'
     end
   end
 end
